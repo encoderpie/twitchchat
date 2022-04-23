7c1567c0f622669fb1f711695135fba0992afb69
encoderpie <coderraenn@gmail.com>
2022-04-23 17:54:14 +0300

    commit

--- scripts/searchChannel.js
+++ scripts/searchChannel.js
@@ -1,5 +1,5 @@
 function searchChannel() {
     let channelNameInput = $('#search_channel').val()
     setCookie('channelname', channelNameInput)
-    window.location.replace("/")
+    location.reload()
 }
